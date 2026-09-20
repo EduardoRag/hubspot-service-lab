db4f27cc3364923b63765481ba36e4fa38decffb
Eduardo <eduardojarek66@gmail.com>
2026-09-20 15:21:44 -0300

    update: add validation for duplicated tickets.

--- src/modules/tickets/ticket.service.ts
+++ src/modules/tickets/ticket.service.ts
@@ -1,5 +1,6 @@
 import { hubSpotClient } from '../../clients/hubspot.client.js';
-import { CreateTicketInput } from './ticket.schemas.js';
+import { HubSpotApiError } from '../../errors/hubspot-api.error.js';
+import type { CreateTicketInput } from './ticket.schemas.js';
 
 export const createTicket = async (input: CreateTicketInput) => {
     const existingTicket =
@@ -11,19 +12,37 @@ export const createTicket = async (input: CreateTicketInput) => {
         return existingTicket.results[0];
     }
 
-    const ticket = await hubSpotClient.createTicket({
-        subject: input.subject,
-        content: input.description,
-        hs_ticket_priority: input.priority,
-        hs_pipeline: '0',
-        hs_pipeline_stage: '1',
-        carrier: input.carrier,
-        issue_category: input.issueCategory,
-        external_system: 'Minha Rota',
-        external_ticket_id: input.externalTicketId,
-    });
+    try {
+        const ticket = await hubSpotClient.createTicket({
+            subject: input.subject,
+            content: input.description,
+            hs_ticket_priority: input.priority,
+            hs_pipeline: '0',
+            hs_pipeline_stage: '1',
+            carrier: input.carrier,
+            issue_category: input.issueCategory,
+            external_system: 'Meu Teste',
+            external_ticket_id: input.externalTicketId,
+        });
 
-    return ticket;
+        return ticket;
+    } catch (error) {
+        if (
+            error instanceof HubSpotApiError &&
+            error.hubSpotStatusCode === 409
+        ) {
+            const ticketCreatedByAnotherRequest =
+                await hubSpotClient.getTicketByExternalId(
+                    input.externalTicketId,
+                );
+
+            if (ticketCreatedByAnotherRequest.total > 0) {
+                return ticketCreatedByAnotherRequest.results[0];
+            }
+        }
+
+        throw error;
+    }
 };
 
 export const getTicket = async (id: string) => {
